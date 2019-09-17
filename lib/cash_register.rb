855b45f7685fa0ee5556d6e519ca3d6d94653a0f
Jennifer Ingram <jennifer.ingram@flatironschool.com>
2019-09-17 12:34:42 -0400

    finished Cash Register lab (class and instance methods)

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,33 @@
+class CashRegister
+    attr_reader :discount
+    attr_accessor :total, :items, :transactions
 
+    def initialize(discount=nil)
+        @total = 0
+        @discount = discount
+        @items = []
+        @transactions = []
+    end
+
+    def add_item(title, price, quantity=1)
+        quantity.times do
+            self.items << title
+            self.transactions << price * quantity
+        end
+        self.total += price * quantity
+    end
+
+    def apply_discount
+        if self.discount
+            self.total = (self.total - (self.total * discount/100))
+            "After the discount, the total comes to $#{self.total}."
+        else
+            "There is no discount to apply."
+        end
+    end
+
+    def void_last_transaction
+        last_transaction = self.transactions.pop
+        self.total = self.total - last_transaction
+    end
+end
